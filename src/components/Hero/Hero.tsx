dc46d2bf02abb48b205e5102eff5889303e25a71
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-02-28 20:31:09 +0200

    Add ! to hero title

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <>
       <h1 className="flex flex-col text-6xl md:text-7xl lg:text-8xl font-bold xl:text-9xl mt-20 mx-auto text-center justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-purple-500 to-purple-600">
         <span>One Task</span>
-        <span>at a Time</span>
+        <span>at a Time!</span>
       </h1>
       <div className="relative h-20 w-full mt-10 max-w-70 mx-auto group">
         <div className="-inset-0.5 flex absolute bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-50 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
